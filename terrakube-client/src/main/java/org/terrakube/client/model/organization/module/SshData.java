2619d5e4b70699e994f1a1658ef5604e6fa4e085
Alfredo España <alfespa17@gmail.com>
2022-08-08 12:59:16 -0600

    Adding missing ssh data in module (#30)

---
+++ terrakube-client/src/main/java/org/terrakube/client/model/organization/module/SshData.java
@@ -0,0 +1,11 @@
+package org.terrakube.client.model.organization.module;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.terrakube.client.model.generic.Resource;
+
+@Getter
+@Setter
+public class SshData {
+    Resource data;
+}
